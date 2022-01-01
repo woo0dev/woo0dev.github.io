ace03395e66685f9c2e542ddc2b7d9644ea3e12c
woo0dev <deongho9732@gmail.com>
2022-01-02 00:20:25 +0900

    swift-closure posting

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -6,7 +6,7 @@ module.exports = {
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
-      repo: ``, // `zoomkoding/zoomkoding-gatsby-blog`,
+      repo: `woo0dev/woo0dev.github.io`, // `zoomkoding/zoomkoding-gatsby-blog`,
     },
   },
   ga: '0', // Google Analytics Tracking ID
